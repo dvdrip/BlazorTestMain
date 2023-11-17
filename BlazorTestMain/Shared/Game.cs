58eab22d15ef6d32f0a74fc77a24d3396ef6f309
David Barredo <datdat@rdmsinc.com>
2023-11-17 16:34:10 +0800

    Add project files.

---
+++ BlazorTestMain/Shared/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTestMain.Shared
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+    }
+}
